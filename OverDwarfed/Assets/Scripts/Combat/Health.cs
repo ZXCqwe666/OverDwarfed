49d0072acd33ab312e154c80d3e16edfc598cc9d
DefinitelyLD <drobnyyalex@gmail.com>
2021-01-29 15:47:56 +0200

    Damageble body

---
+++ OverDwarfed/Assets/Scripts/Combat/Health.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    private const float flashDuration = 0.1f;
+    private int hp;
+    public int maxHp;
+
+    private SpriteRenderer rend;
+    private Material defaultMaterial, whiteFlashMatherial;
+
+    public delegate void EnemyDied();
+    public event EnemyDied enemyDied;
+
+    private void Start()
+    {
+        InitializeHealth();
+        rend = GetComponent<SpriteRenderer>();
+        defaultMaterial = rend.material;
+        whiteFlashMatherial = Resources.Load<Material>("whiteFlash");
+    }
+    public virtual void InitializeHealth()
+    {
+        hp = maxHp;
+    }
+    public void TakeDamage(int _amount)
+    {
+        StartCoroutine(WhiteFlashEffect());
+        hp -= _amount;
+        if (hp <= 0)
+        {
+            enemyDied?.Invoke();
+            Destroy(gameObject);
+        }
+    }
+    public void Heal(int _amount)
+    {
+        hp += _amount;
+        if (hp > maxHp)
+            hp = maxHp;
+    }
+    private IEnumerator WhiteFlashEffect()
+    {
+        rend.material = whiteFlashMatherial;
+        yield return new WaitForSeconds(flashDuration);
+        rend.material = defaultMaterial;
+    }
+}
---
+++ OverDwarfed/Assets/Scripts/Combat/PlayerHealth.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+public class PlayerHealth : MonoBehaviour
+{
+    private const float flashDuration = 0.1f;
+    private int hp;
+    public int maxHp;
+    //public Action characterDied; // maybe death will be handled here too
+
+    private SpriteRenderer rend;
+    private Material defaultMaterial, whiteFlashMatherial;
+
+    private void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        defaultMaterial = rend.material;
+        whiteFlashMatherial = Resources.Load<Material>("whiteFlash");
+        hp = maxHp;
+    }
+    public void TakeDamage(int _amount)
+    {
+        StartCoroutine(WhiteFlashEffect());
+        hp -= _amount;
+        if (hp <= 0)
+            Destroy(gameObject); // DEATH LOGIC
+    }
+    public void Heal(int _amount)
+    {
+        hp += _amount;
+        if (hp > maxHp)
+            hp = maxHp;
+    }
+    private IEnumerator WhiteFlashEffect()
+    {
+        rend.material = whiteFlashMatherial;
+        yield return new WaitForSeconds(flashDuration);
+        rend.material = defaultMaterial;
+    }
+}
+
