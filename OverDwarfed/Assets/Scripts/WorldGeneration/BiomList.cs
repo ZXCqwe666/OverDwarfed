2aa5fcaa337f06a982d63046cb2202ec8ad53ec3
ZXCqwe666 <tlyashkova@gmail.com>
2021-01-30 13:36:42 +0300

    FinalizedGen

--- OverDwarfed/Assets/Scripts/WorldGeneration/BiomList.cs
+++ OverDwarfed/Assets/Scripts/WorldGeneration/BiomList.cs
@@ -17,21 +17,25 @@ namespace MapGeneration
         {
             bioms = new List<Biom>()
             {
-                new Biom (new NoiseData(3, 7f, 0.4f, 1.5f),
+                new Biom (
+                    new NoiseData[] {
+                    new NoiseData (3, 7f, 0.4f, 1.5f),
+                    new NoiseData (3, 8f, 0.4f, 1.5f),
+                    new NoiseData (3, 5f, 0.4f, 2f)},
                     new TileBase[] {
-                    MiningManager.tileTypes[2].destructionStages[3],
+                    MiningManager.tileTypes[0].destructionStages[3],
                     MiningManager.tileTypes[1].destructionStages[3],
-                    MiningManager.tileTypes[0].destructionStages[3], },
-                    new float[] { 0.42f, 0.5f, 1f})
+                    MiningManager.tileTypes[2].destructionStages[3], },
+                    new float[] { 1f, 0.42f, 0.38f})
             };
         }
     }
     public struct Biom // might put 1 player mapSize here and multiplay it by player count
     {
-        public NoiseData noise;
+        public NoiseData[] noise;
         public TileBase[] tileBases;
         public float[] tileSpawnChances;
-        public Biom(NoiseData _noise, TileBase[] _tileBases, float[] _tileSpawnChances)
+        public Biom(NoiseData[] _noise, TileBase[] _tileBases, float[] _tileSpawnChances)
         {
             noise = _noise;
             tileBases = _tileBases;
--- OverDwarfed/Assets/Scripts/WorldGeneration/MapGenerator.cs
+++ OverDwarfed/Assets/Scripts/WorldGeneration/MapGenerator.cs
@@ -18,8 +18,6 @@ namespace MapGeneration
 
 		private Biom curretBiom;
 		private int2 mapSize;
-		private bool[,] map, edgeMap;
-		private float[,] noiseMap;
 
 		public int2 generatedMapSize; // 75,300 medium size
 
@@ -39,17 +37,17 @@ namespace MapGeneration
 
 			curretBiom = BiomList.instance.bioms[biomId];
 			mapSize = _mapSize;
-			map = new bool[mapSize.x, mapSize.y];
-			edgeMap = new bool[mapSize.x, mapSize.y];
-			noiseMap = Noise.GenerateNoiseMap(mapSize, curretBiom.noise);
+			bool[,] map = new bool[mapSize.x, mapSize.y];
+			bool[,] edgeMap = new bool[mapSize.x, mapSize.y];
 
 			RandomMapFill(ref map, randomFillPercent); yield return new WaitForSeconds(genStepTime);
 			RandomMapFill(ref edgeMap, randomFillPercentND); yield return new WaitForSeconds(genStepTime);
 			SmoothMap(ref map, smoothingIterations); yield return new WaitForSeconds(genStepTime);
 		    SmoothMap(ref edgeMap, smoothingIterationsND); yield return new WaitForSeconds(genStepTime);
 
-			FillTiles(); yield return new WaitForSeconds(genStepTime);
+			FillTiles(ref map, ref edgeMap); yield return new WaitForSeconds(genStepTime);
 			FillAreaAround(); yield return new WaitForSeconds(genStepTime);
+			CarveStartingArea(); yield return new WaitForSeconds(genStepTime);
 			MiningManager.instance.InitializeBlockData(mapSize.x, mapSize.y);
 		}
 		private void RandomMapFill(ref bool[,] mapArray, int _randomFillPercent)
@@ -72,23 +70,24 @@ namespace MapGeneration
 				else if (neighbourWallTiles < smoothValue) mapArray[x, y] = false;
 			}
 		}
-		private void FillTiles()
+		private void FillTiles(ref bool[,] map, ref bool[,] edgeMap)
 		{
-			for (int x = 0; x < mapSize.x; x++)
-			for (int y = 0; y < mapSize.y; y++)
-			if (map[x, y] && edgeMap[x, y] == false)
-		    {
-				for (int i = 0; i < curretBiom.tileSpawnChances.Length; i++)
+			for (int i = 0; i < curretBiom.tileBases.Length; i++)
+			{
+				float[,] noiseMap = Noise.GenerateNoiseMap(mapSize, curretBiom.noise[i]);
+
+				for (int x = 0; x < mapSize.x; x++)
+				for (int y = 0; y < mapSize.y; y++)
+				if (map[x, y] && edgeMap[x, y] == false)
+				{
 					if (noiseMap[x, y] <= curretBiom.tileSpawnChances[i])
-					{
-						blockTilemap.SetTile(new Vector3Int(x, y, 0), curretBiom.tileBases[i]);
-						break;
-					}
+					blockTilemap.SetTile(new Vector3Int(x, y, 0), curretBiom.tileBases[i]);
+				}
+				else if (edgeMap[x, y])
+				blockTilemap.SetTile(new Vector3Int(x, y, 0), NonDistructableTile);
 			}
-			else if (edgeMap[x, y]) 
-				blockTilemap.SetTile(new Vector3Int(x, y, 0), NonDistructableTile); 
 		}
-		private int GetSurroundingWallCount(int2 pos, ref bool[,] mapArray)
+		private int GetSurroundingWallCount(int2 pos, ref bool[,] map)
 		{
 			int wallCount = 0;
 			for (int neighbourX = pos.x - 1; neighbourX <= pos.x + 1; neighbourX++)
@@ -97,7 +96,7 @@ namespace MapGeneration
 				{
 					if (neighbourX >= 0 && neighbourX < mapSize.x && neighbourY >= 0 && neighbourY < mapSize.y)
 					{
-						if ((neighbourX != pos.x || neighbourY != pos.y) && mapArray[neighbourX, neighbourY])
+						if ((neighbourX != pos.x || neighbourY != pos.y) && map[neighbourX, neighbourY])
 							wallCount++;
 					}
 					else wallCount++;
@@ -116,6 +115,20 @@ namespace MapGeneration
 			BoundsInt bounds = blockTilemap.cellBounds;
 			Vector3Int fillStart = new Vector3Int(bounds.xMin, bounds.yMin + 1, 0);
 			blockTilemap.BoxFill(fillStart, NonDistructableTile, fillStart.x, fillStart.y, bounds.xMax - 1, bounds.yMax - 2);
+		}
+		private void CarveStartingArea() /// BETA SHIT
+		{
+			int areaDiameter = 15; // 15 10  temp values
+			int2 startPosition = new int2(10, mapSize.y / 2 - areaDiameter / 2);
+
+			for(int x = startPosition.x; x < startPosition.x + areaDiameter; x++)
+			for (int y = startPosition.y; y < startPosition.y + areaDiameter; y++)
+			blockTilemap.SetTile(new Vector3Int(x, y, 0), null);	
+			
+			FindObjectOfType<PlayerController>().transform.position = //// TEMPORARY PLAYER SEARCH
+				new Vector3(startPosition.x + areaDiameter / 2, startPosition.y + areaDiameter / 2, 0f);
+
+		   // ++++ PLACE BUILDINGS AT START
 		}
 	}
 }
