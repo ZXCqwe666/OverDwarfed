f22e5b7bb14fb075bdabe18dfa92d6e763c0423c
DefinitelyLD <drobnyyalex@gmail.com>
2021-02-07 15:28:04 +0200

    crafting tabs

---
+++ OverDwarfed/Assets/BuildingTab.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class BuildingTab : MonoBehaviour
+{
+    public GameObject[] tabs;
+    private Button openTabsButton;
+    private int unlockedTabs;
+    private bool isOpen;
+
+    private void Start()
+    {
+        openTabsButton = GetComponent<Button>();
+        openTabsButton.onClick.AddListener(openTabs);
+    }
+
+    private void openTabs()
+    {
+        isOpen = !isOpen;
+        foreach (GameObject tab in tabs)
+            tab.gameObject.SetActive(isOpen);
+    }
+    private void InitializeBuildingTab()
+    {
+        foreach (GameObject tab in tabs)
+            tab.gameObject.SetActive(false);
+    }
+}
